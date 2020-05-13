7473ea979a752fc37c113c89019708ccfdc37e68
herbertMT <60928553+herbertMT@users.noreply.github.com>
2020-05-13 14:20:09 -0300

    Action move left mouse button

--- Assets/Scripts/NavGame/Managers/Characters/PlayerController.cs
+++ Assets/Scripts/NavGame/Managers/Characters/PlayerController.cs
@@ -9,10 +9,15 @@ public class PlayerController : TouchableGameObject
 {
     NavMeshAgent agent;
     Camera cam;
+
+    public float range = 4f;
+
     public LayerMask walkableLayer;
     public LayerMask collectibleLayer;
 
     CollectibleGameObject pickupTarget;
+
+    Vector3 actionPoint = Vector3.zero;
 
     void Awake()
     {
@@ -20,11 +25,11 @@ public class PlayerController : TouchableGameObject
         cam = Camera.main;
     }
 
-
     void Update()
     {
         ProcessInput();
         UpdateCollect();
+        UpdateAction();
     }
 
     void ProcessInput()
@@ -48,8 +53,20 @@ public class PlayerController : TouchableGameObject
             {
                 pickupTarget = null;
             }
-
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
+            {
+                actionPoint = hit.point;
+                agent.SetDestination(hit.point);
+            }
+        }
+
+
     }
 
     void UpdateCollect()
@@ -59,6 +76,18 @@ public class PlayerController : TouchableGameObject
             if (IsInTouch(pickupTarget))
             {
                 pickupTarget.Pickup();
+            }
+        }
+    }
+
+    void UpdateAction()
+    {
+       if (actionPoint != Vector3.zero)
+        {
+            if(Vector3.Distance(transform.position, actionPoint) <= range)
+            {
+                agent.ResetPath();
+                actionPoint = Vector3.zero;
             }
         }
     }
