026aff59b42f743a3ac130feb17a3b69926b5481
herbertMT <60928553+herbertMT@users.noreply.github.com>
2020-05-14 14:46:36 -0300

    Highlight action

--- Assets/Scripts/Core/Events.cs
+++ Assets/Scripts/Core/Events.cs
@@ -8,6 +8,11 @@ namespace NavGame.Core
     public delegate void OnAttackStrikeEvent(Vector3 strikePoint);
     public delegate void OnDamageTakenEvent(Vector3 strikePoint, int amount);
     public delegate void OnHealthChangedEvent(int maxHealth, int currentHealth);
-    public delegate void OnDiedEvent(); 
+    public delegate void OnDiedEvent();
+
+    // LEVEL EVENTS
+    public delegate void OnActionSelectEvent(int actionIndex);
+    public delegate void OnActionCancelEvent(int actionIndex);
+
 }
 
