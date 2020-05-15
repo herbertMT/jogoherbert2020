cdaa48ab6cd050d77969cafe3fcf38625ec50802
herbertMT <60928553+herbertMT@users.noreply.github.com>
2020-05-14 21:56:01 -0300

    Wave update event

--- Assets/Scripts/Managers/Level01Manager.cs
+++ Assets/Scripts/Managers/Level01Manager.cs
@@ -8,19 +8,35 @@ public class Level01Manager : LevelManager
    public Transform [] badSpawm;
    public GameObject badPrefabs;
    public int badWaves = 3;
+    public int monstersPerWave = 2;
    public float waitTimeFirstWave = 2f;
    public float waitTimeBetweenWaves = 4f;
 
-    
+    protected override void Start()
+    {
+        base.Start();
+        if (onWaveUpdate != null)
+        {
+            onWaveUpdate(badWaves, 0);
+        }
+    }
 
-   protected override IEnumerator SpawnBad()
+    protected override IEnumerator SpawnBad()
    {
        yield return new WaitForSeconds(waitTimeFirstWave);
        for (int i = 0; i < badWaves; i++)
        {
             for (int j = 0; j < badSpawm.Length; j++)
             {
-                Instantiate(badPrefabs, badSpawm[j].position, Quaternion.identity);
+                for (int k = 0; k < monstersPerWave; k++)
+                {
+                    Vector3 offset = new Vector3(Random.Range(-0.1f, 0.1f), 0f, Random.Range(-0.1f, 0.1f));
+                    Instantiate(badPrefabs, badSpawm[j].position, Quaternion.identity);
+                }
+            }
+            if (onWaveUpdate != null)
+            {
+                onWaveUpdate(badWaves, i + 1);
             }
             yield return new WaitForSeconds(waitTimeBetweenWaves);
        }
